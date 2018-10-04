58cead2b9a774f36624825899e413d9ed3c4f41e
iammax <iammax64@gmail.com>
2018-10-04 12:12:47 -0400

    Added a few new metrics to distance_metric.py

--- distance_metric.py
+++ distance_metric.py
@@ -3,6 +3,16 @@
 
 import sys
 import numpy as np
+v1 = np.genfromtxt(sys.argv[1])
+v2 = np.genfromtxt(sys.argv[2])
+
+def l0(v1, v2): #differences
+	dim = len(v1)
+	total = 0
+	for q in range(dim):
+		if v1[q] !=v2[q]:
+			total += 1
+	return total
 
 def l1(v1, v2): #manhattan
 	dim = len(v1)
@@ -18,28 +28,41 @@ def l2(v1, v2): #euclidian distance
 		total += (v2[q]-v1[q])**2
 	return np.sqrt(total)
 
-v1 = np.genfromtxt(sys.argv[1])
-v2 = np.genfromtxt(sys.argv[2])
-N = float(sys.argv[3])
+def linf(v1, v2): #chebyshev
+	dim = len(v1)
+	highest = 0
+	for q in range(dim):
+		diff = abs(v2[q]-v1[q])
+		if diff > highest:
+			highest = diff
+	return highest	
 
 if len(v1) != len(v2):
 	print "Your vectors are of different lengths: {0} for {1}, {2} for {3}".format(len(v1), sys.argv[1], len(v2), sys.argv[2])
 	quit()
-print "Vector 1: {0}".format(v1)
-print "Vector 2: {0}".format(v2)
+print "Vector 1:\n {0}".format(v1)
+print "Vector 2:\n {0}".format(v2)
 
 print """\nEnter the number of the distance metric you want between the two vectors:\n
 0: All of the below
-1: Manhattan distance (l1 norm)
-2: Euclidian distance (l2 norm)"""
+1: l0 norm (number of differences)
+2: Manhattan distance (l1 norm)
+3: Euclidian distance (l2 norm)
+4: Chebyshev distance (l-inf norm)"""
 
-selection = raw_input()
-if selection == '0':
+selection = input('Selection: ')
+if selection == 0:
+	print "Number of differences/L0 norm: ", l0(v1, v2)
 	print 'Manhattan distance/L1 norm: ', l1(v1, v2)
 	print 'Euclidian distance/L2 norm: ', l2(v1, v2)
-elif selection == '1':
+	print 'Chebyshev distance/Linf norm: ', linf(v1, v2)
+elif selection == 1:
+	print "Result: ", l0(v1, v2)
+elif selection == 2:
 	print "Result: ", l1(v1, v2)
-elif selection == '2':
+elif selection == 3:
 	print "Result: ", l2(v1, v2)
+elif selection == 4:
+	print "Result: ", linf(v1, v2)
 else:
 	print "That isn't an option"
